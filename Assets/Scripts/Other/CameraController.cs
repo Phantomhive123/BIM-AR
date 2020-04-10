8200b6d8942564bd8c97cfc9b99fc8cda8d97f6b
Phantomhive123 <33385178+Phantomhive123@users.noreply.github.com>
2020-04-10 20:43:38 +0800

    增加了中键和右键的相机控制

---
+++ Assets/Scripts/Other/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //平移旋转进退
+    private Vector3 originalPos = Vector3.zero;
+    private Vector3 originalRotation = Vector3.zero;
+
+    private Vector3 mousePosLastFrame = Vector3.zero;
+    private Vector3 mousePosCurrentFrame = Vector3.zero;
+
+    [SerializeField]
+    private float HorizontalScaler = 0.01f;//这个系数也需要
+    [SerializeField]
+    private float VerticalScaler = 1f;//感觉这个系数需要动态变化
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = transform.position;
+        originalRotation = transform.rotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(2))
+        {
+            mousePosLastFrame = Input.mousePosition;
+        }
+        if(Input.GetMouseButton(2))
+        {
+            mousePosCurrentFrame = Input.mousePosition;
+            transform.position -= (mousePosCurrentFrame - mousePosLastFrame) * HorizontalScaler;
+            mousePosLastFrame = mousePosCurrentFrame;
+        }
+
+        float scrollWheelMovement = Input.GetAxis("Mouse ScrollWheel");
+        transform.position += transform.forward * scrollWheelMovement * VerticalScaler;
+        
+    }
+}
